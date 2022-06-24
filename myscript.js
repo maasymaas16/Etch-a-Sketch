e222f9ac2e2799eece4026e5ce495f17e8c9d027
maasymaas16 <maasymaas16@gmail.com>
2022-06-24 09:21:12 -0600

    Add mousehover effects and button for user input
    Added mouseover eventlistener to change background color of box when
    mouse passes over it. Also added a button to ask the user for a number
    to generate a 'number x number' grid of boxes.

--- myscript.js
+++ myscript.js
@@ -1,12 +1,33 @@
-const container = document.querySelector('.container');
+let elements = 0;
 
-for (let i = 0; i < 16; i++){
+const container = document.querySelector('.container');
+const btn = document.createElement('button');
+btn.innerHTML = 'Number of Elements';
+container.appendChild(btn);
+
+const button = document.querySelector('button');
+button.addEventListener('click', function (){
+   elements =  parseInt(prompt('Please enter number of elements'));
+   if (elements > 100 ){
+    elements = parseInt(prompt('Please enter a number less than 100'));
+   }
+   for (let i = 0; i < elements; i++){
     const row = document.createElement('div');
     row.classList.add('row')
     container.appendChild(row);
-    for (let j = 0; j < 16; j++){
+    for (let j = 0; j < elements; j++){
         const div = document.createElement('div');
         div.classList.add('box');
         row.appendChild(div);
     }
 }
+
+const boxes = document.querySelectorAll('.box');
+boxes.forEach((box) => {
+    box.addEventListener('mouseover', function () {
+        box.style.backgroundColor = 'black';
+    });
+});
+
+});
+
