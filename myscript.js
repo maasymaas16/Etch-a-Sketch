92576ec4f739a9f80deb418e84b6e70407419de9
maasymaas16 <maasymaas16@gmail.com>
2022-06-25 12:10:07 -0600

    Changed styling of box elements
    Changed the etch-a-sketch elements to be blank boxes and added a grid
    container that is centered on the page. Also changed from mouseover
    to click to change the backgruond color of each element.

--- myscript.js
+++ myscript.js
@@ -4,9 +4,13 @@ const container = document.querySelector('.container');
 const btn = document.createElement('button');
 btn.innerHTML = 'Number of Elements';
 container.appendChild(btn);
+const grid = document.createElement('grid');
+grid.classList.add('grid');
+container.appendChild(grid);
 
 const button = document.querySelector('button');
 button.addEventListener('click', function (){
+    grid.textContent = "";
    elements =  parseInt(prompt('Please enter number of elements'));
    if (elements > 100 ){
     elements = parseInt(prompt('Please enter a number less than 100'));
@@ -14,7 +18,7 @@ button.addEventListener('click', function (){
    for (let i = 0; i < elements; i++){
     const row = document.createElement('div');
     row.classList.add('row')
-    container.appendChild(row);
+    grid.appendChild(row);
     for (let j = 0; j < elements; j++){
         const div = document.createElement('div');
         div.classList.add('box');
@@ -24,7 +28,7 @@ button.addEventListener('click', function (){
 
 const boxes = document.querySelectorAll('.box');
 boxes.forEach((box) => {
-    box.addEventListener('mouseover', function () {
+    box.addEventListener('click', function () {
         box.style.backgroundColor = 'black';
     });
 });
